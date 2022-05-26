c55b16ea7d005f8d54f0b08c234e93c16d581f8e
Sherwin Sandoval <sherwin.sandoval026@gmail.com>
2022-05-26 10:09:49 +0800

    Remove django secret key

--- water_consumption_site/settings.py
+++ water_consumption_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-whf(@**r25mxbf9)xu7=tf8z3yc*#72=z7p7mma(z&9gr5481$'
+# SECRET_KEY = 'django-insecure-whf(@**r25mxbf9)xu7=tf8z3yc*#72=z7p7mma(z&9gr5481$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
