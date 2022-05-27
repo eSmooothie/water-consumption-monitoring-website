83f9a03cd62a97af4abb97c96d3e9e095d5bcbb6
Sherwin Sandoval <sherwin.sandoval026@gmail.com>
2022-05-27 17:30:20 +0800

    Change Debug to False

--- water_consumption_site/settings.py
+++ water_consumption_site/settings.py
@@ -29,7 +29,7 @@ if os.path.isfile(dotenv_file):
 # SECRET_KEY = 'django-insecure-whf(@**r25mxbf9)xu7=tf8z3yc*#72=z7p7mma(z&9gr5481$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
