114d280505ad946d81f644349c06b2ceaf89722f
Sherwin Sandoval <sherwin.sandoval026@gmail.com>
2022-05-29 23:03:25 +0800

    Change peso per cu. m.

--- mysite/views.py
+++ mysite/views.py
@@ -113,7 +113,7 @@ def add_consumption(request):
         elapsed_time = body["elapsed_time"]
         elapsed_time_in_min = convert_sec_to_min(elapsed_time)
         
-        peso_per_cu_m = 0.24
+        peso_per_cu_m = 0.24 #
 
         amount = elapsed_time_in_min * peso_per_cu_m
 
